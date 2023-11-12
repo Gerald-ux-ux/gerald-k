91c5f893f77c10e8d0a1a4901e7ee81a0fa4275e
11geraldmoney <11geraldmoney@gmail.com>
2023-11-12 16:54:53 +0300

    -add documentation & theme setup

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -1,3 +1,7 @@
+/**
+ * I have imported NextThemesProvider to facilitate dark mode
+ * Heres a youtube tutorial: "https://youtu.be/PHDRKXQzyrU?si=f13p36m2-819Nsfk"
+ */
 "use client";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
@@ -7,7 +11,8 @@ export default function ThemeProvider({
   ...props
 }: ThemeProviderProps) {
   return (
-    <NextThemesProvider themes={["light, dark"]} {...props}>
+    /** I have two defined themes. You can have as many as you would like */
+    <NextThemesProvider themes={["light", "dark"]} {...props}>
       {children}
     </NextThemesProvider>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   theme: {
     container: {
@@ -14,44 +15,42 @@ const config: Config = {
         "2xl": "1400px",
       },
     },
-  },
-  extend: {
-    textColor: {
-      primary: "var(--gray-12)",
-      secondary: "var(--gray-11)",
-      tertiary: "var(--gray-9)",
-      brand: "var(--brand)",
-      link: "var(--blue-10)",
-    },
-    backgroundColor: {
-      primary: "var(--gray-1)",
-      secondary: "var(--gray-4)",
-      secondaryA: "var(--gray-a4)",
-      tertiary: "var(--gray-3)",
-    },
-    borderColor: {
-      primary: "var(--gray-6)",
-      secondary: "var(--gray-4)",
-    },
-    ringOffsetColor: {
-      primary: "var(--gray-12)",
-    },
-    keyFrames: {
-      in: {
-        "0%": { transform: "translateY(18px)", opacity: 0 },
-        "100%": { transform: "translateY(0)", opacity: 1 },
+    extend: {
+      textColor: {
+        primary: "var(--gray-12)",
+        secondary: "var(--gray-11)",
+        tertiary: "var(--gray-9)",
+        brand: "var(--brand)",
+        link: "var(--blue-10)",
       },
-      "in-reverse": {
-        "0%": { transform: "translateY(-18px)", opacity: 0 },
-        "100%": { transform: "translateY(0px)", opacity: 1 },
+      backgroundColor: {
+        primary: "var(--gray-1)",
+        secondary: "var(--gray-4)",
+        secondaryA: "var(--gray-a4)",
+        tertiary: "var(--gray-3)",
+      },
+      borderColor: {
+        primary: "var(--gray-6)",
+        secondary: "var(--gray-4)",
+      },
+      ringOffsetColor: {
+        primary: "var(--gray-12)",
+      },
+      keyframes: {
+        in: {
+          "0%": { transform: "translateY(18px)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+        "in-reverse": {
+          "0%": { transform: "translateY(-18px)", opacity: 0 },
+          "100%": { transform: "translateY(0px)", opacity: 1 },
+        },
+      },
+      animation: {
+        in: "in .6s both",
+        "in-reverse": "in-reverse .6s both",
       },
     },
-    animations: {
-      in: "in .6s both",
-      "in-reverse": "in-reverse .6s both",
-    },
   },
-
   plugins: [require("@tailwindcss/typography")],
 };
-export default config;
