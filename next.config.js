5940ad59721e350516a926245c0464725a9b735c
11geraldmoney <11geraldmoney@gmail.com>
2023-11-15 20:34:28 +0300

    -add blogs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  domain: [],
-};
+const { withContentlayer } = require("next-contentlayer");
 
-module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = withContentlayer(nextConfig);
