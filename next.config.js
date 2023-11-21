2ac32f443e9e44a7dbf22b8b8f007311fc3609c3
Gerald kamau <109945073+Gerald-ux-ux@users.noreply.github.com>
2023-11-21 11:40:14 +0300

    Revert "Gerald"

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {};
 
 module.exports = withContentlayer(nextConfig);
