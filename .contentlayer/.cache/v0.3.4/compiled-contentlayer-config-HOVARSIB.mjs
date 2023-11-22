76e766fdd652a4d372eb0164030e4ff17d097992
11geraldmoney <11geraldmoney@gmail.com>
2023-11-22 07:04:59 +0300

    -fix resolving merge conflicts
    fixed the merge conflicts

---
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-HOVARSIB.mjs
@@ -0,0 +1,76 @@
+// contentlayer.config.ts
+import {
+  defineDocumentType,
+  makeSource
+} from "contentlayer/source-files";
+import rehypePrism from "rehype-prism-plus";
+import rehypeSlug from "rehype-slug";
+var getSlug = (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "");
+var postComputedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => getSlug(doc)
+  },
+  image: {
+    type: "string",
+    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`
+  },
+  og: {
+    type: "string",
+    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`
+  }
+};
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: "mdx",
+  /**Fields */
+  fields: {
+    title: { type: "string", required: true },
+    summary: { type: "string", required: true },
+    publishedAt: { type: "string", required: true },
+    updatedAt: { type: "string", required: false },
+    tags: { type: "json", required: false },
+    featured: { type: "boolean", required: false },
+    shortTitle: { type: "string", required: false, default: "" }
+  },
+  computedFields: postComputedFields
+}));
+var codeSnippetsComputedFields = {
+  slug: {
+    type: "string",
+    resolve: (doc) => getSlug(doc)
+  },
+  image: {
+    type: "string",
+    resolve: (doc) => `/projects/${getSlug(doc)}/image.png`
+  }
+};
+var CodeSnippets = defineDocumentType(() => ({
+  name: "Code snippets",
+  filePathPattern: `code-snippets/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: { type: "string", required: true },
+    summary: { type: "string", required: true },
+    publishedAt: { type: "string", required: true },
+    updatedAt: { type: "string", required: false },
+    tags: { type: "json", required: false },
+    featured: { type: "boolean", required: false },
+    shortTitle: { type: "string", required: false, default: "" }
+  },
+  computedFields: codeSnippetsComputedFields
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "content",
+  documentTypes: [Post, CodeSnippets],
+  mdx: {
+    rehypePlugins: [rehypePrism, rehypeSlug]
+  }
+});
+export {
+  CodeSnippets,
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-HOVARSIB.mjs.map
