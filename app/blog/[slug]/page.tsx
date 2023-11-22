be1dca08ab4e49de84fd874cc468182ab204c08c
11geraldmoney <11geraldmoney@gmail.com>
2023-11-22 08:13:09 +0300

    -fix vercel error & -add page slug
    -M

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,9 +1,6 @@
 /** This file is used to generate dynamic routes
  * See more (https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes)
  */
-/** This file is used to generate dynamic routes
- * See more (https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes)
- */
 import { Metadata, ResolvingMetadata } from "next";
 import { allPosts, Post as PostType } from "../../../.contentlayer/generated";
 import { notFound } from "next/navigation";
@@ -11,15 +8,10 @@ import Image from "next/image";
 import Avatar from "@/public/geraldavatar.jpeg";
 import { formatDate } from "@/lib/formatdate";
 import ViewCounter from "../components/ui/ViewCounter";
-
 type PostProps = {
   post: PostType;
   related: PostType[];
 };
-
-/** Generating dynamic metadata
- * See more (https://nextjs.org/docs/app/api-reference/functions/generate-metadata)
- */
 
 /** Generating dynamic metadata
  * See more (https://nextjs.org/docs/app/api-reference/functions/generate-metadata)
@@ -38,7 +30,6 @@ export async function generateMetadata(
   parent: ResolvingMetadata,
 ): Promise<Metadata> {
   // read route parameters
-  // read route parameters
   const post = allPosts.find((post) => post.slug === params.slug);
 
   if (!post) {
@@ -53,8 +44,7 @@ export async function generateMetadata(
     slug,
   } = post;
 
-  // Missing the image field.
-  // Missing the image field.
+  // Missing the image field
   const metadata: Metadata = {
     title: `${title} | Gerald`,
     description,
@@ -79,47 +69,45 @@ export default async function Post({ params }: { params: any }) {
 
   return (
     <div className="flex flex-col gap-20">
-      <div className="flex flex-col gap-20">
-        <article>
-          <div className="flex animate-in flex-col gap-8">
-            <div className="max-w-xl space-y-2">
-              <h1 className="text-3xl font-bold leading-tight tracking-tight text-primary">
-                {post.title}
-              </h1>
-              <p className="text-lg leading-tight text-secondary md:text-xl">
-                {post.summary}
+      <article>
+        <div className="flex animate-in flex-col gap-8">
+          <div className="max-w-xl space-y-2">
+            <h1 className="text-3xl font-bold leading-tight tracking-tight text-primary">
+              {post.title}
+            </h1>
+            <p className="text-lg leading-tight text-secondary md:text-xl">
+              {post.summary}
+            </p>
+          </div>
+
+          <div className="flex max-w-none items-center gap-4">
+            {/* Might change the sizing of the img */}
+            <Image
+              src={Avatar}
+              alt="avatar-img"
+              width={40}
+              height={40}
+              className="rounded-full bg-secondary"
+            />
+
+            <div className="leading-light">
+              <p className="font-medium text-primary">Gerald Kamau</p>
+              <p>
+                <time dateTime={post.publishedAt}>
+                  {formatDate(post.publishedAt)}
+                </time>
+
+                {post.updatedAt
+                  ? `(updated ${formatDate(post.publishedAt)})`
+                  : ""}
+
+                {" . "}
+                <ViewCounter post={post} />
               </p>
             </div>
-
-            <div className="flex max-w-none items-center gap-4">
-              {/* Might change the sizing of the img */}
-              <Image
-                src={Avatar}
-                alt="avatar-img"
-                width={40}
-                height={40}
-                className="rounded-full bg-secondary"
-              />
-
-              <div className="leading-light">
-                <p className="font-medium text-primary">Gerald Kamau</p>
-                <p>
-                  <time dateTime={post.publishedAt}>
-                    {formatDate(post.publishedAt)}
-                  </time>
-
-                  {post.updatedAt
-                    ? `(updated ${formatDate(post.publishedAt)})`
-                    : ""}
-
-                  {" . "}
-                  <ViewCounter post={post} />
-                </p>
-              </div>
-            </div>
           </div>
-        </article>
-      </div>
+        </div>
+      </article>
     </div>
   );
 }
--- lib/formatdate.ts
+++ lib/formatdate.ts
@@ -5,4 +5,3 @@ export const formatDate = (date: string) => {
     year: "numeric",
   });
 };
-
