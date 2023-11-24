a563bd9cec435e281ae51e85322199135937c61d
11geraldmoney <11geraldmoney@gmail.com>
2023-11-24 17:52:32 +0300

    -fix deployment error

--- app/about/components/ConnectSection.tsx
+++ app/about/components/ConnectSection.tsx
@@ -1,9 +1,24 @@
 import Section from "@/components/Section";
+import Link from "next/link";
 
 export default function ConnectSection(){
     return (
-        <Section heading="Connect" headingAlignment="left">
-
-        </Section>
-    )
+      <Section heading="Connect" headingAlignment="left">
+        <p>
+          In addition to coding, I also make{" "}
+          <Link
+            className="underline"
+            href="https://www.youtube.com/channel/@brianruizy"
+          >
+            YouTube
+          </Link>{" "}
+          videos, where I focus on tech gear, creative vlogs, and a bit of
+          personal development.
+        </p>
+        <p>
+          When I&apos;m not at my desk I am probably lifting weights, playing
+          soccer, or at a coffee shop
+        </p>
+      </Section>
+    );
 }
--- app/about/components/WorkSection.tsx
+++ app/about/components/WorkSection.tsx
@@ -1,9 +1,24 @@
 import Section from "@/components/Section";
+import Link from "next/link";
 
 export default function WorkSection (){
     return (
-        <Section heading="Work" headingAlignment="left">
-
-        </Section>
-    )
+      <Section heading="Work" headingAlignment="left">
+        <p>
+          In addition to coding, I also make{" "}
+          <Link
+            className="underline"
+            href="https://www.youtube.com/channel/@brianruizy"
+          >
+            YouTube
+          </Link>{" "}
+          videos, where I focus on tech gear, creative vlogs, and a bit of
+          personal development.
+        </p>
+        <p>
+          When I&apos;m not at my desk I am probably lifting weights, playing
+          soccer, or at a coffee shop
+        </p>
+      </Section>
+    );
 }
