85e2e699afd969c8111e8180aafdad91e215eedb
11geraldmoney <11geraldmoney@gmail.com>
2023-11-28 13:15:41 +0300

    -update work section

--- app/about/components/WorkSection.tsx
+++ app/about/components/WorkSection.tsx
@@ -1,24 +1,46 @@
 import Section from "@/components/Section";
 import Link from "next/link";
+import WorkPlaces from "./WorkPlaces";
+import cblogo from "@/public/work/cblogo.png";
+import cloudpaylogo from "@/public/work/cplogo.png";
 
-export default function WorkSection (){
-    return (
-      <Section heading="Work" headingAlignment="left">
+export default function WorkSection() {
+  return (
+    <Section heading="Work" headingAlignment="left">
+      <div className="flex w-full flex-col gap-8">
         <p>
-          In addition to coding, I also make{" "}
-          <Link
-            className="underline"
-            href="https://www.youtube.com/channel/@brianruizy"
-          >
-            YouTube
-          </Link>{" "}
-          videos, where I focus on tech gear, creative vlogs, and a bit of
-          personal development.
+          {new Date().getFullYear() - 2022} + year(s) of professional software
+          development experience.
         </p>
+
         <p>
-          When I&apos;m not at my desk I am probably lifting weights, playing
-          soccer, or at a coffee shop
+          I kicked off my tech adventure by immersing myself in an intensive
+          bootcamp, mastering the foundational aspects of software development.
+          This journey led me to a rewarding role as a full-stack software
+          developer, with a special focus on front-end expertise at{" "}
+          <Link href="https://www.creativebrands.co.ke/" className="underline">
+            Creative Brands
+          </Link>
         </p>
-      </Section>
-    );
+        <WorkPlaces items={workplaces} />
+      </div>
+    </Section>
+  );
 }
+
+const workplaces = [
+  {
+    title: "Full Stack Engineer",
+    company: "Creative Brands",
+    time: "2022 -",
+    imageSrc: cblogo,
+    link: "https://www.creativebrands.co.ke/",
+  },
+  {
+    title: "Fronted end software Engineer",
+    company: "Cloudpay Finance",
+    time: "2022 - ",
+    imageSrc: cloudpaylogo,
+    link: "https://cloudpay.finance/",
+  },
+];
