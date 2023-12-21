5f74d881f590d5b4915caf33b2a8546c9c3884dd
11geraldmoney <11geraldmoney@gmail.com>
2023-12-21 07:49:23 +0300

    fix: youtube api response

--- app/api/youtube/route.ts
+++ app/api/youtube/route.ts
@@ -1,14 +1,24 @@
+let cachedETag: string | undefined;
+
 export async function GET() {
   if (process.env.YOUTUBE_API_KEY === undefined) {
     return new Response("No API key specified", { status: 500 });
   }
   const url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UCAszclBzNqvwJpM4F1OdhXQ&key=${process.env.YOUTUBE_API_KEY}`;
-
+  const headers = new Headers();
+  if (cachedETag) {
+    headers.set("If-None-Match", cachedETag);
+  }
   try {
-    const res = await fetch(url);
+    const res = await fetch(url, { headers });
+
     const data = await res.json();
     const subs = data.items[0].statistics.subscriberCount;
     const views = data.items[0].statistics.viewCount;
+    if (res.status === 304) {
+      return Response.json({ subs, views });
+    }
+    cachedETag = res.headers.get("etag")!;
     return Response.json({ subs, views });
   } catch (error) {
     return new Response(`Something went wrong: ${error}`, { status: 200 });
