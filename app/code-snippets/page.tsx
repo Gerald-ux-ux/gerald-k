4ff7e7e9cd8715d17e2caf721b594af7051363b3
Gerald-G <11geraldmoney@gmail.com>
2024-01-20 01:54:55 +0300

    feat: adding a tooltip msg

--- app/code-snippets/page.tsx
+++ app/code-snippets/page.tsx
@@ -3,11 +3,28 @@ import Construction from "@/public/images/construction.jpg";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+// import * as Tooltip from "@radix-ui/react-tooltip";
 
 export const metadata: Metadata = {
   title: "Code-snippets | Gerald",
-  description: "Under construction...",
+  description: "Coming soon ",
 };
+
+//   const ToolTipDemo = () => {
+//     return (
+//       <Tooltip.Provider>
+//         <Tooltip.Root>
+//           <Tooltip.Trigger asChild></Tooltip.Trigger>
+//           <Tooltip.Portal>
+//             <Tooltip.Content className="TooltipContent" sideOffset={5}>
+//               Add to library
+//               <Tooltip.Arrow className="TooltipArrow" />
+//             </Tooltip.Content>
+//           </Tooltip.Portal>
+//         </Tooltip.Root>
+//       </Tooltip.Provider>
+//     );
+//   };
 
 export default function CodeSnippets() {
   return (
@@ -15,17 +32,17 @@ export default function CodeSnippets() {
       <div className="flex  w-full items-center justify-between">
         <h1 className={PAGE_HEADER}>Code snippets</h1>
 
-        <button className="rounded-lg md:text-base text-sm bg-secondaryA p-2 text-primary hover:text-secondary">
+        <button className="rounded-lg bg-secondaryA p-2 text-sm text-primary hover:text-secondary md:text-base">
           Add a snippet
         </button>
       </div>
 
       <div>
         <h2 className="mb-2 flex animate-in items-center gap-4 text-lg font-semibold text-primary md:text-xl">
-          This page is still under development. I am Currently working on the ui
-          design and backend of the feature. The whole feature will be released
-          late next week and users will be able to add code snippets and share
-          their ideas with other developers.
+          This page is still under development. I am Currently working on the Ui
+          and Backend design. The whole feature will be released late next week
+          and users will be able to add code snippets and share their ideas with
+          other developers.
         </h2>
         <Link
           href="/"
