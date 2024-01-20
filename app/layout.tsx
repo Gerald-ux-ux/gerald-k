55f94a0bb35a3d0d2d135993b44730b64218f563
Gerald-G <11geraldmoney@gmail.com>
2024-01-21 01:25:40 +0300

    feat: adding the tooltip

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import Navigation from "@/components/Navigation";
 import { Analytics } from "@vercel/analytics/react";
 import ThemeProvider from "@/components/ThemeProvider";
 import Footer from "@/components/ui/Footer";
+import { Tooltip } from "react-tooltip";
 
 const inter = Inter({ subsets: ["latin"] });
 
