a520f60fc58c836731cc07008cc5b1e30225376c
Gerald-G <11geraldmoney@gmail.com>
2024-02-04 16:04:26 +0300

    feat: building the sign up

--- app/auth/components/AuthInputs.ts
+++ app/auth/components/AuthInputs.ts
@@ -1,6 +1,12 @@
-"use client"
+"use client";
 
 export const loginInputs = [
+  { type: "string", placeholder: "Email", value: "", onChange: () => {} },
+  { type: "password", placeholder: "Password", value: "", onChange: () => {} },
+];
+
+export const signupInputs = [
+  { type: "string", placeholder: "Name", value: "", onChange: () => {} },
   { type: "string", placeholder: "Email", value: "", onChange: () => {} },
   { type: "password", placeholder: "Password", value: "", onChange: () => {} },
 ];
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,7 +1,28 @@
-export default function SignUp () {
-    return (
-        <div>
-            <h1>Sign Up</h1>
+import { signupInputs } from "../components/AuthInputs";
+import Button from "../components/Button";
+import Form from "../components/Form";
+import Info from "../components/Info";
+import TagLine from "../components/TagLine";
+
+export default function SignUp() {
+  return (
+    <div className="flex animate-in flex-col items-center justify-center md:mt-20  ">
+      {/* <CustomMessage text="Log in to add a snippet" /> */}
+      <div className="flex flex-col items-center   justify-center gap-6">
+        <TagLine />
+
+        <div className="flex w-full flex-col gap-8">
+          <Form inputs={signupInputs} />
+
+          <Button label="Sign up" action="" />
+
+          <Info
+            text="Already have an account"
+            action="Sign up"
+            href="/auth/login"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
