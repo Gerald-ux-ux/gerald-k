e51f30731cb202419cd75bdf9321e7583e103071
Gerald-G <11geraldmoney@gmail.com>
2024-02-28 16:47:22 +0300

    refactor: updating to next content layer

--- app/code-snippets/page.tsx
+++ app/code-snippets/page.tsx
@@ -38,6 +38,7 @@ export default async function CodeSnippets() {
 
         <div className="flex flex-col gap-12">
           <Search data={snippets} />
+
           <Snippets data={snippets} />
         </div>
       </main>
