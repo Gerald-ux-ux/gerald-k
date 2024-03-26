afeca656cc9b7e3631f08ad19ea34418c7011b76
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-03-26 16:01:58 +0300

    fix: updating the middleware auth logic

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const isAuth = req.cookies.has("auth");
-  const requestedPath = req.nextUrl.pathname;
+  const auth = req.cookies.has("auth");
+  const { pathname, origin } = req.nextUrl;
 
-  if (
-    isAuth &&
-    (requestedPath === "/auth/login" || requestedPath === "/auth/signup")
-  ) {
-    return NextResponse.redirect(
-      // Protecting the auth route
-      process.env.NODE_ENV !== "production"
-        ? "http://localhost:3000/code-snippets?"
-        : "https://gerald-k.vercel.app/code-snippets",
-    );
+  const isAuthPage = pathname === "/auth/login" || pathname === "/auth/signup";
+
+  if (isAuthPage && auth) {
+    return NextResponse.redirect(`${origin}/code-snippets`);
   }
+
 
   return NextResponse.next();
 }
