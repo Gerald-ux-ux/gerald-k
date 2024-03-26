94c3fa60f5e89be455c993c9428dfbb9860fecf2
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-03-26 15:51:56 +0300

    fix: returning the prev conent layer version

--- .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-XCGD4DZL.mjs
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-XCGD4DZL.mjs
@@ -1,27 +1,22 @@
-import { ComputedFields, defineDocumentType, makeSource } from "contentlayer/source-files";
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
 import GithubSlugger from "github-slugger";
-// eslint-disable-line
 import rehypePrism from "rehype-prism-plus";
 import rehypeSlug from "rehype-slug";
-
-const getSlug = (doc: any) => doc._raw.sourceFileName.replace(/\.mdx$/, "");
-
-/** Post definition */
-
-const postComputeFields: ComputedFields = {
+var getSlug = (doc) => doc._raw.sourceFileName.replace(/\.mdx$/, "");
+var postComputeFields = {
   slug: {
     type: "string",
-    resolve: (doc) => getSlug(doc),
+    resolve: (doc) => getSlug(doc)
   },
   image: {
     type: "string",
-    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`,
+    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`
   },
   og: {
     type: "string",
-    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`,
+    resolve: (doc) => `/blog/${getSlug(doc)}/image.png`
   },
-
   /**This section is not necessary. I personally added it to make the UX(user-experience) better so people can easily jump into a heading title
    * References: ["https://delba.dev/blog/next-blog-structured-mdx-content-with-contentlayer#complete-code", "https://youtu.be/YC6LqIYVHxI?si=iyYVOlqRuAXSpjiS"]
    * Explanation: {
@@ -35,32 +30,24 @@ const postComputeFields: ComputedFields = {
     resolve: async (doc) => {
       const slugger = new GithubSlugger();
       const regXHeader = /\n\n(?<flag>#{1,6})\s+(?<content>.+)/g;
-
-      // Check if doc.body.raw is defined and non-empty
       if (doc.body.raw && doc.body.raw.trim() !== "") {
         const matches = Array.from(doc.body.raw.matchAll(regXHeader));
-
-        // Use map on the array of matches
-        const headings = matches.map(({ groups }: any) => {
+        const headings = matches.map(({ groups }) => {
           const flag = groups?.flag;
           const content = groups?.content;
           return {
             heading: flag?.length,
             text: content,
-            slug: content ? slugger.slug(content) : undefined,
+            slug: content ? slugger.slug(content) : void 0
           };
         });
-
         return headings;
       }
-
-      // Return an empty array if there are no headings
       return [];
-    },
-  },
+    }
+  }
 };
-
-export const Post = defineDocumentType(() => ({
+var Post = defineDocumentType(() => ({
   name: "Post",
   contentType: "mdx",
   filePathPattern: `blog/**/*.mdx`,
@@ -71,26 +58,21 @@ export const Post = defineDocumentType(() => ({
     updatedAt: { type: "string", required: false },
     tags: { type: "json", required: false },
     featured: { type: "boolean", required: false },
-    shortTitle: { type: "string", required: false },
+    shortTitle: { type: "string", required: false }
   },
-
-  computedFields: postComputeFields,
+  computedFields: postComputeFields
 }));
-
-/** Projects definition */
-
-const projectComputeFields: ComputedFields = {
+var projectComputeFields = {
   slug: {
     type: "string",
-    resolve: (doc) => getSlug(doc),
+    resolve: (doc) => getSlug(doc)
   },
   image: {
     type: "string",
-    resolve: (doc) => `/projects/${getSlug(doc)}/image.png`,
-  },
+    resolve: (doc) => `/projects/${getSlug(doc)}/image.png`
+  }
 };
-
-export const Project = defineDocumentType(() => ({
+var Project = defineDocumentType(() => ({
   name: "Project",
   contentType: "mdx",
   filePathPattern: `project/**/*.mdx`,
@@ -99,17 +81,21 @@ export const Project = defineDocumentType(() => ({
     description: { type: "string", required: true },
     time: { type: "string", required: true },
     url: { type: "string", required: false },
-    tags: { type: "json", required: false },
+    tags: { type: "json", required: false }
   },
-  computedFields: projectComputeFields,
+  computedFields: projectComputeFields
 }));
-
-export default makeSource({
+var contentlayer_config_default = makeSource({
   contentDirPath: "content",
-
   documentTypes: [Post, Project],
   mdx: {
     // @ts-expect-error
-    rehypePlugins: [rehypePrism, rehypeSlug],
-  },
+    rehypePlugins: [rehypePrism, rehypeSlug]
+  }
 });
+export {
+  Post,
+  Project,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-XCGD4DZL.mjs.map
