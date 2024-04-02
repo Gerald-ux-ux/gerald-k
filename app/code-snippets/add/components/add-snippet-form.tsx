13e1261890d8b291f6f7d08b4bce9868a2cd4700
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-02 16:40:53 +0300

    feat: add snippet form

--- app/code-snippets/add/components/add-snippet-form.tsx
+++ app/code-snippets/add/components/add-snippet-form.tsx
@@ -1,10 +1,25 @@
 "use client";
+import { AUTH_BTN } from "@/app/auth/styles/authStyles";
+import BtnLoader from "@/components/ui/btn-loader";
+import { PlusIcon } from "@heroicons/react/20/solid";
+import { TrashIcon } from "@heroicons/react/24/outline";
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import { useFormStatus } from "react-dom";
+import clsx from "clsx";
 
 const inputClass =
   "w-full rounded-md border border-primary bg-secondary p-2 focus:border-none";
 const editorClass = "rounded-lg p-4";
 
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button disabled={pending} className={clsx(AUTH_BTN, "mt-4")}>
+      {pending ? <BtnLoader /> : "Post snippet 😀"}
+    </button>
+  );
+}
 export default function Form() {
   return (
     <form action="" className="flex w-full flex-col gap-4">
@@ -16,7 +31,14 @@ export default function Form() {
         className={inputClass}
       />
       <span className="border border-primary" />
+
       <span className=" w-full p-2">
+        <input
+          type="text"
+          className="w-full rounded-md border-t border-primary bg-secondary p-2 focus:border-none"
+          placeholder="Code heading"
+        />
+
         <Editor
           className="rounded-lg"
           height="25vh"
@@ -25,10 +47,25 @@ export default function Form() {
           defaultValue="//Hello world!"
         />
 
-        {/* <span className="mt-12 w-full border-t border-primary bg-secondary">
-          Select Language
-        </span> */}
+        <div className="flex w-full items-center justify-between rounded-md border-t border-primary bg-secondary p-2 focus:border-none">
+          <span className="">Select Language</span>
+          <button className="flex ">
+            <TrashIcon width={20} height={20} />
+          </button>
+        </div>
       </span>
+      <button className="flex items-center gap-2  text-primary">
+        Add another snippet
+        <PlusIcon width={20} height={20} />
+      </button>
+
+      <input
+        type="text"
+        className={inputClass}
+        placeholder="Add tags up to 3 max (e.g JavaScript, Python, React.Js)"
+      />
+
+      <Button />
     </form>
   );
 }
