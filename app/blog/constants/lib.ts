d9edbd016271a9ae3a8f6fe82737aa896a6bfabe
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-08 10:44:47 +0300

    fix: removing / api

--- app/blog/constants/lib.ts
+++ app/blog/constants/lib.ts
@@ -1,5 +1,5 @@
 import { baseUrl } from "@/app/api/baseUrl";
 
-export const Total_Blog_Views = `${baseUrl}/api/blogs/views`;
+export const Total_Blog_Views = `${baseUrl}/blogs/views`;
 export const Specific_Blog_Views = (slug: string) =>
-  `${baseUrl}/api/blogs/views/${slug}`;
+  `${baseUrl}/blogs/views/${slug}`;
