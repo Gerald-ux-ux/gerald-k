0deb3a6eef9dd060e9b081ab67aac38913b5e5f7
Gerald-ux-ux <11geraldmoney@gmail.com>
2024-04-10 15:14:19 +0300

    refactor: changing from local hoast to baseurl

--- app/code-snippets/constants/lib.ts
+++ app/code-snippets/constants/lib.ts
@@ -1,10 +1,10 @@
 import { baseUrl } from "@/app/api/baseUrl";
 
-export const GET_SNIPPETS = `http://localhost:8008/api/code-snippets/get`;
+export const GET_SNIPPETS = ` ${baseUrl}/api/code-snippets/get`;
 export const Give_Feedback = `${baseUrl}/api/code-snippets/feedback`;
 export const Add_Snippet = `${baseUrl}/api/code-snippets/add`;
-export const Copy_Snippet = `http://localhost:8008/api/code-snippets/copy`;
-export const Delete_Snippet = `http://localhost:8008
+export const Copy_Snippet = `${baseUrl}api/code-snippets/copy`;
+export const Delete_Snippet = `${baseUrl}
 /api/code-snippets/delete`;
 export const Delete_Code = (id: string) =>
-  `http://localhost:8008/api/code-snippets/delete/${id}`;
+  `${baseUrl}/api/code-snippets/delete/${id}`;
